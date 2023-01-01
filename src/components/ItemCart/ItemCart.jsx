a3bf385f10aa9d6aa0acd7191a430c9e59ba4b58
Nadia Santiago <nadusantiago@gmail.com>
2022-12-31 21:21:20 -0300

    productos carrito

---
+++ src/components/ItemCart/ItemCart.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useCartContext } from '../../context/CartContext'
+import './ItemCart.scss'
+const ItemCart = ({producto}) => {
+    const {eliminarProducto}= useCartContext()
+  return (
+    <div className='producto-cart'>
+        <div className='cart-titulos'>
+            <p>Producto</p>
+            <p>Cantidad</p>
+            <p>Precio un</p>
+            <p>Subtotal</p>
+        </div>
+        <div className='cart-detail'>
+            <img src={producto.img} alt={producto.nombre} />
+            <p>{producto.nombre}</p>
+            <p>{producto.cantidad}</p>
+            <p>${producto.precio}</p>
+            <p>${producto.precio * producto.cantidad}</p>
+            <button onClick={()=> eliminarProducto(producto.id)}>Eliminar</button>
+        </div>
+    </div>
+  )
+}
+
+export default ItemCart
