f7cdf564da547fb9a3225460a9eb02577a9b0262
Nadia Santiago <nadusantiago@gmail.com>
2023-01-10 10:10:06 -0300

    order status funcionando

--- src/components/OrderStatus/OrderStatus.jsx
+++ src/components/OrderStatus/OrderStatus.jsx
@@ -20,42 +20,44 @@ const OrderStatus = () => {
   }, [orderId])
 
   const orderInexistente = () => {
-    <div>
-      <h2> Orden {orderId} inexistente, por favor verifique el numero de orden</h2>
-      <Link to={'/'}>Volver a la tienda</Link>
-    </div>
   }
   const orderExistente = ()=>{
-    <div>
-      <div>
-        <h1>N° Orden: {orderId}</h1>
-        <h2>¡Estamos trabajando en su pedido!</h2>
-      </div>
-      <div>
-        <p>Cliente: {orderData.buyer.nombre} {orderData.buyer.apellido}</p>
-        <p>Direccion de entrega: {orderData.buyer.direccion}</p>
-        <p>Correo: {orderData.buyer.email}</p>
-        <p>Telefono: {orderData.buyer.telefono}</p>
-      </div>
-      <div>
-        {
-        orderData.items.map((item) => (
-          <div key={item.id}>
-            <img src={item.img} alt={item.name}/>
-            <p>{item.name}</p>
-            <p>${item.precio} / {item.quantity} Un.</p>
-          </div>
-        ))
-        }
-      </div>
-      <div>
-        <p>Total: {orderData.total}</p>
-      </div>
-    </div>
   }
 
   return (
-    orderData ? orderInexistente :  orderExistente
+    orderData == undefined ? (
+      <div>
+        <h2> Orden {orderId} inexistente, por favor verifique el numero de orden</h2>
+        <Link to={'/'}>Volver a la tienda</Link>
+      </div>
+    ) : (
+      <div>
+        <div>
+          <h1>N° Orden: {orderId}</h1>
+          <h2>¡Estamos trabajando en su pedido!</h2>
+        </div>
+        <div>
+          <p>Cliente: {orderData.buyer.nombre} {orderData.buyer.apellido}</p>
+          <p>Direccion de entrega: {orderData.buyer.direccion}</p>
+          <p>Correo: {orderData.buyer.email}</p>
+          <p>Telefono: {orderData.buyer.telefono}</p>
+        </div>
+        <div>
+          {
+          orderData.items.map((item) => (
+            <div key={item.id}>
+              <img src={item.img} alt={item.name}/>
+              <p>{item.name}</p>
+              <p>${item.precio} / {item.quantity} Un.</p>
+            </div>
+          ))
+          }
+        </div>
+        <div>
+          <p>Total: {orderData.total}</p>
+        </div>
+    </div>
+    )
   )
 }
 
